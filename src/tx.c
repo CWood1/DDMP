a3f61f229b631244e7f2b65cc7770e3c30f28d7a
CWood1 <connorwood71@gmail.com>
2014-08-30 12:34:27 +0000

    Moved sending of all heartbeats to its own function in TX

--- src/tx.c
+++ src/tx.c
@@ -44,6 +44,37 @@ lHeartbeat* sendHeartbeat(int sd, struct sockaddr_in addr, tStream* pcStream, in
 	return s;
 }
 
+int sendHeartbeats(struct timeval* last, int flags, int sd, struct sockaddr_in bcastaddr,
+		struct sockaddr_in directaddr, tStream* pcStream) {
+	struct timeval now;
+	gettimeofday(&now, NULL);
+
+	if(last->tv_sec == 0 ||
+			last->tv_sec < now.tv_sec || (now.tv_usec - last->tv_usec) >= 100000) {
+		lHeartbeat* sent;
+
+		if(flags & TXFLAGS_BCAST) {
+			sent = sendHeartbeat(sd, bcastaddr, pcStream, flags);
+
+			if(sent == NULL) {
+				return 1;
+			}
+
+			free(sent);
+		}
+
+		sent = sendHeartbeat(sd, directaddr, pcStream, flags & ~(TXFLAGS_BCAST));
+
+		if(sent == NULL)
+			return 2;
+
+		*last = sent->timeSent;
+		free(sent);
+	}
+
+	return 0;
+}
+
 void* txmain(void* stream) {
 	int sd, rc, len;
 	struct sockaddr_in bcastaddr, directaddr;
@@ -106,35 +137,17 @@ void* txmain(void* stream) {
 	last.tv_sec = 0;
 
 	while(1) {
-		struct timeval now;
-		gettimeofday(&now, NULL);
-
-		if(last.tv_sec == 0 ||
-			last.tv_sec < now.tv_sec || (now.tv_usec - last.tv_usec) >= 100000) { 
-			lHeartbeat* sent;
-
-			if(bcastFlag == 1) {
-				sent = sendHeartbeat(sd, bcastaddr, pcStream, 1);
-
-				if(sent == NULL) {
-					printf("Unable to send broadcast heartbeat.\n");
-					close(sd);
-					pthread_exit(NULL);
-				}
-
-				free(sent);
-			}
-
-			sent = sendHeartbeat(sd, directaddr, pcStream, 0);
-
-			if(sent == NULL) {
+		switch(sendHeartbeats(&last, bcastFlag, sd, bcastaddr, directaddr, pcStream)) {
+			case 1:
+				printf("Unable to send broadcast heartbeat.\n");
+				close(sd);
+				pthread_exit(NULL);
+				break;
+			case 2:
 				printf("Unable to send direct heartbeat.\n");
 				close(sd);
 				pthread_exit(NULL);
-			}
-
-			last = sent->timeSent;
-			free(sent);
+				break;
 		}
 
 		char* cmd = stream_rcv_nblock(cmdStream, &len);
--- src/tx.h
+++ src/tx.h
@@ -1,6 +1,8 @@
 #ifndef __TX_H__
 #define __TX_H__
 
+#define TXFLAGS_BCAST 1
+
 void* txmain(void*);
 
 #endif
